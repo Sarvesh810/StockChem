ae570b72272983e72328665fb66cda0336e687bb
Sarvesh Khandelwal <sarveshkhandelwal710@gmail.com>
2024-11-29 09:47:11 +0530

    ADD Firebase login setup

---
+++ stock_chem/web/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzBzg4AA6Ba5oF0Q6Xl8QdBFJ1VhhvnsM",
+  authDomain: "stock-chem-firebase.firebaseapp.com",
+  projectId: "stock-chem-firebase",
+  storageBucket: "stock-chem-firebase.firebasestorage.app",
+  messagingSenderId: "977590044316",
+  appId: "1:977590044316:web:e011386e953b6384c28f59"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
